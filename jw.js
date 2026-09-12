3dc62459c119dc3645bab3b53b858ab01df2ef93
BytePayVE <group.info.and.comm@gmail.com>
2026-09-12 00:40:17 -0400

    Update caching strategy to use CACHE_NAME

--- jw.js
+++ jw.js
@@ -1,7 +1,15 @@
+const CACHE_NAME = 'bytepay-cache-v1';
+const assetsToCache = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('bytepay-store').then((cache) => {
-      return cache.addAll(['./index.html']);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
     })
   );
 });
@@ -13,4 +21,3 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
-
